58f99caa5ab05c48d5a821811618b6aea3ac3264
Brucey <woollybah@gmail.com>
2020-03-17 15:37:13 +0000

    New Linux release.

--- website/pages/en/downloads.js
+++ website/pages/en/downloads.js
@@ -95,15 +95,15 @@ class Downloads extends React.Component {
                                 {
                                     content:
                                         `<strong>
-                                            <a href="https://github.com/bmx-ng/bmx-ng/releases/download/v0.116.3.41.linux.x64/BlitzMax_linux_x64_0.116.3.41.tar.xz">BlitzMax_linux_x64_0.116.3.41.tar.xz</a>
+                                            <a href="https://github.com/bmx-ng/bmx-ng/releases/download/v0.118.3.41.linux.x64/BlitzMax_linux_x64_0.118.3.41.tar.xz">BlitzMax_linux_x64_0.118.3.41.tar.xz</a>
                                          </strong>
                                         <br/><br/>
                                         BlitzMax, Linux edition, aimed at x64 development, with x64 MaxIDE.
-                                        Includes SDL backend, mojo2, Crypto, Audio and Gtk MaxGUI.
+                                        Includes SDL backend, mojo2, Crypto, Audio, Text, Steam and Gtk MaxGUI.
                                         `,
                                     image: `${siteConfig.baseUrl}img/platform/linux.svg`,
                                     imageAlign: "top",
-                                    imageLink: `https://github.com/bmx-ng/bmx-ng/releases/download/v0.116.3.41.linux.x64/BlitzMax_linux_x64_0.116.3.41.tar.xz`,
+                                    imageLink: `https://github.com/bmx-ng/bmx-ng/releases/download/v0.118.3.41.linux.x64/BlitzMax_linux_x64_0.118.3.41.tar.xz`,
                                     title: "Linux x64"
                                 },
                                 {},
